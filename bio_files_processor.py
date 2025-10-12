278f4cd0ed119adc2904ee0bf8967ea4041a2e21
perepelicas <ross8marquise@gmail.com>
2025-10-12 23:44:25 +0300

    Edit according to pep8

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -1,7 +1,9 @@
 import os
 import re
 
-def convert_multiline_fasta_to_one_line(input_fasta : str, output_fasta : str | None = None) -> None:
+
+def convert_multiline_fasta_to_one_line(input_fasta : str,
+                                        output_fasta : str | None = None) -> None:
     """
     Converts multiline sequences in FASTA files to a single line.
 
@@ -18,7 +20,8 @@ def convert_multiline_fasta_to_one_line(input_fasta : str, output_fasta : str | 
         raise ValueError("The input file cannot be overwritten.")
     if os.path.exists(output_fasta):
         raise FileExistsError("File already exists.")
-    with open(input_fasta, 'r') as input_file, open(output_fasta, 'a') as output_file:
+    with open(input_fasta, 'r') as input_file,
+         open(output_fasta, 'a') as output_file:
         header = None
         sequence = []
         for line in input_file:
@@ -56,7 +59,7 @@ def parse_blast_output(input_file : str, output_file : str) -> None:
     while i < len(lines):
         line = lines[i].strip()
         if line == "Sequences producing significant alignments:":
-            i += 3 # Skipping the blank line and table header
+            i += 3  # Skipping the blank line and table header
             if i < len(lines):
                 match = lines[i].strip()
                 split_match = re.split("... |  ", match)
@@ -68,4 +71,3 @@ def parse_blast_output(input_file : str, output_file : str) -> None:
     with open(output_file, 'w') as output:
         for match in best_matches:
             output.write(f"{match}\n")
-
